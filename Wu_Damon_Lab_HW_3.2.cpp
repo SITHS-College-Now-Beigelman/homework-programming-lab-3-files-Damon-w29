3fa9e11604fcd28c53701e2fd13c26b718c11b55
Damon-w29 <Damonw18@nycstudents.net>
2024-10-15 17:50:45 -0400

    part 2

---
+++ Wu_Damon_Lab_HW_3.2.cpp
@@ -0,0 +1,20 @@
+// Damon Wu
+// 10/15/2024
+// HW 3 pt 2 
+
+#include<iostream>
+#include<string>
+#include<iomanip>
+#include<fstream>
+
+using namespace std;
+
+int main(){
+    ifstream infile;
+    ofstream offile;
+
+    infile.open("moni.txt");
+    offile.open("statements.txt");
+
+return 0;
+}
